4ab6f500fcb057a37bb52fa84c6c45c22dd3ff1a
ab-e1 <abelghy17@gmail.com>
2026-07-24 15:01:22 +0300

    used a while loops to solve reverse string

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  let length = Number(str.length);
+  let i = length - 1;
+  let reversedStr = "";
+  while (i >= 0) {
+    reversedStr = reversedStr + str[i];
+    i--;
+  }
+  return reversedStr;
 };
 
 // Do not edit below this line
