8688c980bff883538542782c72c38e1b75619960
ab-e1 <abelghy17@gmail.com>
2026-07-24 14:29:39 +0300

    done with repeatString.js, and also i used the while loop

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  if (num < 0) {
+    returnError;
+  } else {
+    let strings = "";
+    let i = 0;
+    while (i < num) {
+      strings = strings + str;
+      i++;
+    }
+    return strings;
+  }
 };
 
 // Do not edit below this line
